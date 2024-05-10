8b62037e89361a359c34e2c7188ed7615bf9474e
Sahithyan <sahithyan2701@gmail.com>
2024-05-10 09:07:55 +0530

    refactor: remove unwanted detector firstBytes

--- lib/detector.ts
+++ lib/detector.ts
@@ -5,16 +5,7 @@ const keys = Object.keys(typeHandlers) as imageType[]
 
 // This map helps avoid validating for every single image type
 const firstBytes: { [byte: number]: imageType } = {
-  0x38: 'psd',
-  0x42: 'bmp',
-  0x44: 'dds',
-  0x47: 'gif',
-  0x49: 'tiff',
-  0x4d: 'tiff',
-  0x52: 'webp',
-  0x69: 'icns',
   0x89: 'png',
-  0xff: 'jpg',
 }
 
 export function detector(input: Uint8Array): imageType | undefined {
