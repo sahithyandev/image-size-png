d3a358d2543fb2735a2f050e7e6e1d65926d8a13
Sahithyan <sahithyan2701@gmail.com>
2024-05-10 11:24:56 +0530

    chore: add tests for unsupported file types

---
+++ specs/unsupported.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai'
+import { sync as globSync } from 'glob'
+import { imageSize } from '../lib'
+
+// Test all invalid files
+describe('Unsupported images', () => {
+  const unsupportedFiles = globSync('specs/images/unsupported/**/*.*')
+
+  unsupportedFiles.forEach((file) => {
+    describe(file, () => {
+      it('should throw when called synchronously', () => {
+        expect(() => imageSize(file)).to.throw(TypeError, 'unsupported')
+      })
+
+      it('should callback with error when called asynchronously', (done) => {
+        imageSize(file, (e) => {
+          expect(e).to.be.instanceOf(TypeError)
+          expect(e?.message).to.match(/^unsupported .+$/)
+          done()
+        })
+      })
+    })
+  })
+})
